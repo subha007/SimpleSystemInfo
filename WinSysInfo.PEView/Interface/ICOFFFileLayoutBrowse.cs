380c03c3f4c2f04c0b9f137d13148aef1d9ef12a
sndjones007 <subhadeep.niogi@gmail.com>
2016-02-02 21:35:36 +0000

    New Additions

---
+++ WinSysInfo.PEView/Interface/ICOFFFileLayoutBrowse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinSysInfo.PEView.Interface
+{
+    internal interface ICOFFFileLayoutBrowse
+    {
+        void Read(IFileReadStrategy readStrategy);
+    }
+}
---
+++ WinSysInfo.PEView/Process/Browse/FileBrowser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinSysInfo.PEView.Factory;
+using WinSysInfo.PEView.Helper;
+using WinSysInfo.PEView.Interface;
+using WinSysInfo.PEView.Model;
+
+namespace WinSysInfo.PEView.Process
+{
+    /// <summary>
+    /// The root class used to browse a file in the file browser tree
+    /// </summary>
+    public class FileBrowser
+    {
+        /// <summary>
+        /// All the browser properties for browsing
+        /// </summary>
+        public FileBrowserProperty Property { get; set; }
+    }
+}
